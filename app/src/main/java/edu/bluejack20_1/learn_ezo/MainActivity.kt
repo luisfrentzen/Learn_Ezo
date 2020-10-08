616741bbf7c54052926500dc53bd687d133382e7
Luis <luisfrentzen@gmail.com>
2020-10-08 18:54:42 +0700

    Resolved material component error

--- app/src/main/java/edu/bluejack20_1/learn_ezo/MainActivity.kt
+++ app/src/main/java/edu/bluejack20_1/learn_ezo/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.example.tpa_mobile.LoginActivity
+import edu.bluejack20_1.learn_ezo.LoginActivity
 
 
 class MainActivity : AppCompatActivity() {
