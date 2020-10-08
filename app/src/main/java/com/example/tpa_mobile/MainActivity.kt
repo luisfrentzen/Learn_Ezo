d8365f06607c4e6cd7bac0c39475aec62a6d3a7a
Luis <luisfrentzen@gmail.com>
2020-10-08 16:05:01 +0700

    added login button

--- app/src/main/java/com/example/tpa_mobile/MainActivity.kt
+++ app/src/main/java/com/example/tpa_mobile/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.tpa_mobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
