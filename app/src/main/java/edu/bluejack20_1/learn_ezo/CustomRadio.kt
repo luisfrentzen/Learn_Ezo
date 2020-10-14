b74c3dbc81466b97cda26542d691979b94d46f85
Luis <luisfrentzen@gmail.com>
2020-10-14 20:49:20 +0700

    Added user preferences

---
+++ app/src/main/java/edu/bluejack20_1/learn_ezo/CustomRadio.kt
@@ -0,0 +1,45 @@
+package edu.bluejack20_1.learn_ezo
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.CompoundButton
+import androidx.appcompat.widget.AppCompatRadioButton
+
+
+class CustomRadio : AppCompatRadioButton {
+    var onChangeListener : OnCheckedChangeListener? = null
+
+
+    constructor(context: Context?) : super(context)
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(
+        context: Context?,
+        attrs: AttributeSet?,
+        defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr)
+
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        setOwnOnCheckedChangeListener()
+        buttonDrawable = null //lets remove the default drawable to create our own
+    }
+
+
+    fun setOwnOnCheckedChangeListener(onCheckedChangeListener: OnCheckedChangeListener?) {
+        this.onChangeListener = onCheckedChangeListener
+    }
+
+    private fun setOwnOnCheckedChangeListener() {
+        setOnCheckedChangeListener(object : OnCheckedChangeListener {
+            override fun onCheckedChanged(
+                buttonView: CompoundButton?,
+                isChecked: Boolean
+            ) {
+                onChangeListener?.onCheckedChanged(buttonView, isChecked)
+            }
+        })
+    }
+}
--- app/src/main/java/edu/bluejack20_1/learn_ezo/SetupActivity.kt
+++ app/src/main/java/edu/bluejack20_1/learn_ezo/SetupActivity.kt
@@ -2,10 +2,23 @@ package edu.bluejack20_1.learn_ezo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 class SetupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup)
+
+        loadFragment(SetPreferencesFragment())
+    }
+
+    fun loadFragment(childFragment : Fragment){
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fl_setup, childFragment).addToBackStack(null).commit()
+    }
+
+    fun finishSetup(){
+
     }
 }
