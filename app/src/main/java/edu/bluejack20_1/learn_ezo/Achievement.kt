5ab2c719ed374cf7ead21f9981d85f8adf93aada
Kimberly Atalya <kimberlyatte@yahoo.com>
2020-10-15 15:43:48 +0700

    updated achievement page

--- app/src/main/java/edu/bluejack20_1/learn_ezo/Achievement.kt
+++ app/src/main/java/edu/bluejack20_1/learn_ezo/Achievement.kt
@@ -2,19 +2,35 @@ package edu.bluejack20_1.learn_ezo
 
 import java.io.Serializable
 
-class Achievement constructor(
-    id : Int,
-    title : String,
-    desc : String,
-    currentProgress : Int,
-    targetProgress : Int,
-    icon : Int) : Serializable{
 
-    var achievement_id = id
-    var achievement_title = title
-    var achievement_desc = desc
-    var achievement_current_progress = currentProgress
-    var achievement_target_proggress = targetProgress
-    var achievement_icon = icon
+class Achievement : Serializable{
+
+    var id: Int ?= null
+    var title: String ?= null
+    var desc: String ?= null
+    var currentProgress: Int ?= null
+    var goal: Int ?= null
+    var icon: String ?= null
+
+    constructor(
+        id : Int,
+        title : String,
+        desc : String,
+        currentProgress: Int,
+        targetProgress : Int,
+        icon : String){
+
+        this.id = id
+        this.title = title
+        this.desc = desc
+        this.currentProgress = currentProgress
+        this.goal = targetProgress
+        this.icon = icon
+
+    }
+
+    constructor(){
+
+    }
 
 }
